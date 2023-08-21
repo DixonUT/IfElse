b0a95829e49839e57c1d1e027e2f46bebd3b37c2
DixonUT <136409468+DixonUT@users.noreply.github.com>
2023-08-20 19:10:24 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+
+/*
+// Variables
+let truthy = "I am a string";
+const IsTrue = true;
+const IsFalse = false;
+const IsNull = null;
+const IsUndef = undefined;
+const Iszero = 0
+const IsEmpty = "";
+
+// If value is truthy print true
+if (truthy === IsTrue) {console.log ("True")};
+//if value is falsy
+if (truthy === IsFalse) {console.log ('The Boolean Value False is Falsey')};
+if (truthy === IsNull) {console.log ('The Null Value is Falsey')};
+if (truthy === IsUndef) {console.log ('Undefined is Falsey')};
+if (truthy === Iszero){console.log ('The Number 0 Is Falsey')};
+if (truthy === IsEmpty){console.log ('The Empty String is Falsey')};
+
+console.log (truthy)
+*/
+
+ /*let num1 = 50
+let num2 = 51
+const sum = num1 + num2
+
+if (sum === -1000) { console.log(sum + ' is a negative number'); }
+else if (sum < 0) { console.log(sum + ' is a negative number'); }
+else if (sum === 0) { console.log(sum + ' is equal to 0'); }
+else if (sum > 100) { console.log(sum + ' is greater than 100'); }
+else { console.log  (sum + ' is greater than 0');} */
+/*
+let num1 = 100
+let num2 = -1000
+
+if (num1 & num2 >=5){console.log ('True');}
+else {console.log ('False');}
+*/
+
+let param1a = 'cat';
+let param1b = 'cat';
+let param2a = 6
+let param2b = '6'
+
+if (param1a === param1b || param2a === param2b) {console.log ('true');}
+else {console.log (false);}
